1a79120709977dc2930c4d749e013acc8fd08a14
최준영 <chad1994@skuniv.ac.kr>
2019-01-19 00:09:58 +0900

    #기능추가 제작년도 기간 설정

--- app/src/main/java/com/example/yeseul/movieapp/mapper/MovieMapper.java
+++ app/src/main/java/com/example/yeseul/movieapp/mapper/MovieMapper.java
@@ -8,15 +8,21 @@ public class MovieMapper {
     public static final String QUERY_STRING_SEARCH_KEY = "query";
     public static final String QUERY_STRING_PAGE_UNIT = "display";
     public static final String QUERY_STRING_START = "start";
+    public static final String QUERY_STRING_YEAR_FROM = "yearfrom";
+    public static final String QUERY_STRING_YEAR_TO = "yearto";
 
-    public static Map<String, String> toRequest(String searchKey, int pageUnit, int start){
+    public static Map<String, String> toRequest(String searchKey, int pageUnit, int start, int yearFrom, int yearTo, boolean isActiveYear) {
 
         Map<String, String> queryMap = new HashMap<>();
         queryMap.put(QUERY_STRING_SEARCH_KEY, searchKey);
-        queryMap.put(QUERY_STRING_PAGE_UNIT, ""+pageUnit);
-        queryMap.put(QUERY_STRING_START, ""+start);
+        queryMap.put(QUERY_STRING_PAGE_UNIT, "" + pageUnit);
+        queryMap.put(QUERY_STRING_START, "" + start);
+
+        if (isActiveYear) {
+            queryMap.put(QUERY_STRING_YEAR_FROM, "" + yearFrom);
+            queryMap.put(QUERY_STRING_YEAR_TO, "" + yearTo);
+        }
 
         return queryMap;
     }
-
 }
