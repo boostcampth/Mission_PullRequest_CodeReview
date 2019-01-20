8661bc9c572a8a3d002ddbfc032866d58ca81432
카퀴카퀴 <kokoxg2@naver.com>
2019-01-20 22:51:58 +0900

    Commit2 Interface 에 있는 상수들을 Factory 로 변경 (IntDef 사용)

--- app/src/main/java/com/example/yeseul/movieapp/data/model/MovieSortStrategy.java
+++ app/src/main/java/com/example/yeseul/movieapp/data/model/MovieSortStrategy.java
@@ -9,11 +9,5 @@ import java.util.List;
 
 public interface MovieSortStrategy {
 
-    public static final int DICTIONARY_ORDER = 0;
-    public static final int LATEST_ORDER = 1;
-    public static final int OLDEST_ORDER = 2;
-    public static final int HIGHEST_RATING_ORDER = 3;
-    public static final int LOWEST_RATING_ORDER = 4;
-
     List<Movie> sort(List<Movie> list);
 }
--- app/src/main/java/com/example/yeseul/movieapp/data/model/MovieStrategyFactory.java
+++ app/src/main/java/com/example/yeseul/movieapp/data/model/MovieStrategyFactory.java
@@ -1,8 +1,27 @@
 package com.example.yeseul.movieapp.data.model;
 
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import static com.example.yeseul.movieapp.data.model.MovieSortStrategy.*;
 
 public class MovieStrategyFactory {
+
+    // Interface 에 있던 상수들을 Factory Class 로 이동
+    // 넘어오는 매개변수가 정확한 타입인지 인증하기 위해 IntDef 사용
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({DICTIONARY_ORDER, LATEST_ORDER, OLDEST_ORDER, HIGHEST_RATING_ORDER, LOWEST_RATING_ORDER})
+
+    public @interface SortTypeDef {}
+
+    public static final int DICTIONARY_ORDER = 0;
+    public static final int LATEST_ORDER = 1;
+    public static final int OLDEST_ORDER = 2;
+    public static final int HIGHEST_RATING_ORDER = 3;
+    public static final int LOWEST_RATING_ORDER = 4;
+
     public static MovieSortStrategy create(int idx) {
 
         switch (idx) {
